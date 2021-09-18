fd7bed804ba25c769283ce6582118a5f9bff76d9
Lukian <lukian359@gmail.com>
2021-09-18 14:11:02 +0200

    entered coordinates validation

--- src/com/lukian/GameProcess.java
+++ src/com/lukian/GameProcess.java
@@ -12,7 +12,7 @@ import static com.lukian.Constants.PLAYER_INFO;
 
 public class GameProcess {
 
-    private final Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
     private int currentPlayerIndex;
     private int targetPlayerIndex;
     private boolean wasHit;
@@ -20,6 +20,7 @@ public class GameProcess {
     private final List<Player> players;
 
     public GameProcess() {
+        scanner = new Scanner(System.in);
         players = Arrays.asList(
                 new Player(currentPlayerIndex, playerName(currentPlayerIndex)),
                 new Player(currentPlayerIndex, playerName(currentPlayerIndex)));
@@ -67,14 +68,14 @@ public class GameProcess {
                 players.get(currentPlayerIndex).getMap().draw(false);
 
                 System.out.println("begin X and Y: ");
-                begin = new Point(scanner.nextInt(), scanner.nextInt());
+                begin = new Point(coordinate(), coordinate());
 
                 //check is it 1-cell boat
                 if (i == 1) {
                     end = begin;
                 } else {
                     System.out.println("end X and Y: ");
-                    end = new Point(scanner.nextInt(), scanner.nextInt());
+                    end = new Point(coordinate(), coordinate());
                 }
 
                 boat = new Boat(begin, end);
@@ -99,12 +100,22 @@ public class GameProcess {
 
         players.get(targetPlayerIndex).getMap().draw(true);
         wasHit = players.get(targetPlayerIndex).getMap()
-                .hit(new Point(scanner.nextInt(), scanner.nextInt()));
+                .hit(new Point(coordinate(), coordinate()));
     }
 
     //checking boat size
     private boolean isSizeOk(Boat boat, int size) {
         return boat.getPointCount() == size;
+    }
+
+    private int coordinate() {
+        try {
+            return scanner.nextInt();
+        } catch (Exception e) {
+            scanner = new Scanner(System.in);
+            System.out.println("Please try again.");
+            return coordinate();
+        }
     }
 
 }
